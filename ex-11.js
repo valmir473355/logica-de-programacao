2c1e8a0d1af15a0b3f5b5565732df362e9a69f3c
valmir developer <valmirdeveloper@gmail.com>
2023-12-04 00:24:52 -0300

    //Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100.///

--- ex-11.js
+++ ex-11.js
@@ -1,1 +1,1 @@
-//Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100.//
+//Faça um programa onde toda vez que ele rodar, coloca na tela um número aleatório entre 1 e 100.///
