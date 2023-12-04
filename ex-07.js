2db290d492e146cd80e7873116d1d46a506cb5d3
valmir developer <valmirdeveloper@gmail.com>
2023-12-04 00:21:05 -0300

    //Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém os dois nomes devem estar em um Array. Ex: const array = [João, Maria]. João e João, imprime true. João e Maria, imprime false.///

--- ex-07.js
+++ ex-07.js
@@ -1,1 +1,1 @@
-//Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém os dois nomes devem estar em um Array. Ex: const array = ["João", "Maria"]. João e João, imprime true. João e Maria, imprime false.///
+//Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Porém os dois nomes devem estar em um Array. Ex: const array = ["João", "Maria"]. João e João, imprime true. João e Maria, imprime false.////
